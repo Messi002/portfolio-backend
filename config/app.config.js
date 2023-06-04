3a8e2713c04a862a6cc5bb8b89cd0eb0e602df4e
Messi002 <rostandmessi2@gmail.com>
2023-06-04 22:00:24 +0100

    backend commit

---
+++ config/app.config.js
@@ -0,0 +1,11 @@
+
+require('dotenv').config;
+
+const MONGO_DB_CONFIG = {
+    DB: 'mongodb+srv://rostandmessi2:p9bTVuZ9VVsW5IeJ@reactfrontendproject.v2qe9lv.mongodb.net/ReactFrontendProject?retryWrites=true&w=majority'
+}
+
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
