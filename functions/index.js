d8172d7cb2eeab8500df412ce397f606f32fddf8
Messi002 <rostandmessi2@gmail.com>
2023-06-05 00:36:59 +0100

    backend commit

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const { MONGO_DB_CONFIG } = require("./config/app.config");
+const { MONGO_DB_CONFIG } = require("../config/app.config");
 const cors = require('cors');
+const serverless = require("serverless-http");
 
 // Connection to MongoDB
 mongoose.connect(MONGO_DB_CONFIG.DB, {
@@ -25,7 +26,10 @@ app.use(cors());
 app.use(express.json());
 
 // Define a route to fetch the data
-app.use('/api', require("./router/app.route"));
+app.use('/.netlify/functions/api', require("../router/app.route"));
+
+module.exports.handler = serverless(app);
+
 
 
 // Start the server
