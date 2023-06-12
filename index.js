66346353d93cb57ba2ac565c12f65a98dea16370
Messi002 <rostandmessi2@gmail.com>
2023-06-12 15:35:13 +0100

    added code

--- index.js
+++ index.js
@@ -5,17 +5,7 @@ const { MONGO_DB_CONFIG } = require("./config/app.config");
 const cors = require('cors');
 
 // Connection to MongoDB
-mongoose.connect(MONGO_DB_CONFIG.DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log(MONGO_DB_CONFIG.DB);
-    console.error('Error connecting to MongoDB:', error);
-  });
+
 
 
 // Enable CORS
