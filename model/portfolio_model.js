c5a96aead0e2cbc1ad6290cdc903378098afacd4
Messi002 <rostandmessi2@gmail.com>
2023-06-13 16:39:40 +0100

    added code

--- model/portfolio_model.js
+++ model/portfolio_model.js
@@ -1,15 +1,3 @@
 const mongoose = require("mongoose");
 
 const portfolioSchema = new mongoose.model('projects', 
-mongoose.Schema({
-  _id: String,
-  image: String,
-  title: String,
-  github: String,
-  demo: String
-}))
-
-
-module.exports = {
-    portfolioSchema
-}
